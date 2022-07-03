fc8068055ffd1349f1f2b556e0de292672259c4a
unknown <pivnev_tosno@mail.ru>
2022-07-03 17:53:47 +0300

    new repository

---
+++ Code/001_GPIO/001_Led_button.c
@@ -0,0 +1,45 @@
+#include "debug.h"
+/*-----------------------------------------------------------*/
+void GPIO_Toggle_INIT(void)
+{
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    GPIO_InitTypeDef GPIO_Output_pin = {0};
+    //Configure the GPIO PC.0 Led pin
+    GPIO_Output_pin.GPIO_Pin = GPIO_Pin_0;
+    GPIO_Output_pin.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_Output_pin.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_Init(GPIOC, &GPIO_Output_pin);
+    GPIOC->BSHR = GPIO_Pin_0;//Set PC.0 high level(1)
+    //Configure the GPIO PA.0 button pin
+    GPIO_InitTypeDef GPIO_Input_pin = {0};
+    GPIO_Input_pin.GPIO_Pin = GPIO_Pin_0;
+    GPIO_Input_pin.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(GPIOA, &GPIO_Input_pin);
+}
+/*-----------------------------------------------------------*/
+int main(void)
+{
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	GPIO_Toggle_INIT();
+	Delay_Init();
+	USART_Printf_Init(115200);
+	printf("SystemClk:%d\r\n",SystemCoreClock);
+	printf("This is printf example\r\n");
+	printf("Hello world\r\n");
+
+	while(1)
+    {
+	if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)!=1){
+	    GPIOC->OUTDR ^=GPIO_Pin_0;
+	    Delay_Ms(250);
+	    GPIOC->OUTDR ^=GPIO_Pin_0;
+	    Delay_Ms(250);
+	    printf("Blink Led pin PC.0\r\n");
+	    }
+	else{
+	    GPIOC->BSHR = GPIO_Pin_0;//Set PC.0 high level(1)
+	}
+	}
+}
+/*-----------------------------------------------------------*/
