22ecf3dcf4f4566b4a5e4f605b9d97a2fcc99c24
unknown <pivnev_tosno@mail.ru>
2022-07-04 15:42:35 +0300

    edited the repository

--- Code/001_GPIO/001_Led_button.c
+++ Code/001_GPIO/001_Led_button.c
@@ -1,16 +1,19 @@
 #include "debug.h"
 /*-----------------------------------------------------------*/
+uint8_t Count = 0;
 void GPIO_Toggle_INIT(void)
 {
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOC, ENABLE);
     GPIO_InitTypeDef GPIO_Output_pin = {0};
-    //Configure the GPIO PC.0 Led pin
-    GPIO_Output_pin.GPIO_Pin = GPIO_Pin_0;
+    //Configure the GPIO PC.0, PC.1, PC.2, PC.3 --> Led1, Led2, ELLed1, ELLed2 pin
+    GPIO_Output_pin.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_2|GPIO_Pin_3;
     GPIO_Output_pin.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_Output_pin.GPIO_Speed = GPIO_Speed_2MHz;
     GPIO_Init(GPIOC, &GPIO_Output_pin);
     GPIOC->BSHR = GPIO_Pin_0;//Set PC.0 high level(1)
+    GPIOC->BSHR = GPIO_Pin_1;//Set PC.1 high level(1)
+    GPIOC->BSHR = GPIO_Pin_2;//Set PC.2 high level(1)
+    GPIOC->BSHR = GPIO_Pin_3;//Set PC.3 high level(1)
     //Configure the GPIO PA.0 button pin
     GPIO_InitTypeDef GPIO_Input_pin = {0};
     GPIO_Input_pin.GPIO_Pin = GPIO_Pin_0;
@@ -27,19 +30,49 @@ int main(void)
 	printf("SystemClk:%d\r\n",SystemCoreClock);
 	printf("This is printf example\r\n");
 	printf("Hello world\r\n");
-
-	while(1)
-    {
+	while(1){
 	if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)!=1){
-	    GPIOC->OUTDR ^=GPIO_Pin_0;
-	    Delay_Ms(250);
-	    GPIOC->OUTDR ^=GPIO_Pin_0;
-	    Delay_Ms(250);
-	    printf("Blink Led pin PC.0\r\n");
-	    }
+	    switch(Count){
+	    case 0:
+        GPIOC->OUTDR ^=GPIO_Pin_0;
+        Delay_Ms(250);
+        GPIOC->OUTDR ^=GPIO_Pin_0;
+        Delay_Ms(250);
+        printf("Blink Led pin PC.0\r\n");
+        Count++;
+        break;
+        case 1:
+        GPIOC->OUTDR ^=GPIO_Pin_1;
+        Delay_Ms(250);
+        GPIOC->OUTDR ^=GPIO_Pin_1;
+        Delay_Ms(250);
+        printf("Blink Led pin PC.1\r\n");
+        Count++;
+        break;
+        case 2:
+        GPIOC->OUTDR ^=GPIO_Pin_2;
+        Delay_Ms(250);
+        GPIOC->OUTDR ^=GPIO_Pin_2;
+        Delay_Ms(250);
+        printf("Blink Led pin PC.2\r\n");
+        Count++;
+        break;
+        case 3:
+        GPIOC->OUTDR ^=GPIO_Pin_3;
+        Delay_Ms(250);
+        GPIOC->OUTDR ^=GPIO_Pin_3;
+        Delay_Ms(250);
+        printf("Blink Led pin PC.3\r\n");
+        Count=0;
+        break;
+	  }
+	}
 	else{
 	    GPIOC->BSHR = GPIO_Pin_0;//Set PC.0 high level(1)
-	}
+	    GPIOC->BSHR = GPIO_Pin_1;//Set PC.1 high level(1)
+	    GPIOC->BSHR = GPIO_Pin_2;//Set PC.2 high level(1)
+	    GPIOC->BSHR = GPIO_Pin_3;//Set PC.3 high level(1)
+	  }
 	}
 }
 /*-----------------------------------------------------------*/
---
+++ README.md
@@ -0,0 +1,20 @@
+## CH32V307VCT6 RISC-V MCU (CH32V307V-EVT-R1 ).  
+
+
+
+
+
+## Контакты ##  
+* :left_speech_bubble: E-mail:  pivnev_tosno@mail.ru  
+
+
+
+
+
+
+
+ 
+
+
+
+
